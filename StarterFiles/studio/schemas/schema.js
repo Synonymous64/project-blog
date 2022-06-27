dd922266dedc0703a308d58b007d85a611d617c5
Synonymous64 <prajwalurkude007@gmail.com>
2022-06-27 17:58:37 +0530

    initial commit

---
+++ StarterFiles/studio/schemas/schema.js
@@ -0,0 +1,9 @@
+/* eslint-disable */
+import createSchema from 'part:@sanity/base/schema-creator';
+import schemaTypes from 'all:part:@sanity/base/schema-type';
+/* eslint-enable */
+
+export default createSchema({
+  name: 'default',
+  types: schemaTypes.concat([]),
+});
---
+++ StarterFiles/web/gatsby-config.js
@@ -0,0 +1,10 @@
+require('dotenv').config('./.env');
+
+module.exports = {
+  siteMetadata: {
+    title: `TechHub-blog`,
+    siteUrl: `https://techhub-blog.netlify.com`,
+    description: `TechHub Blog is a platform for latest technology news and updates.`,
+  },
+  plugins: [],
+};
