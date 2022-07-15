4a314da62501fa209c072e48004f17ec0e8d5c7a
Synonymous64 <prajwalurkude007@gmail.com>
2022-07-15 20:25:08 +0530

    Blog Pages

--- StarterFiles/web/src/components/TypoGraphy/MyPortableText.js
+++ StarterFiles/web/src/components/TypoGraphy/MyPortableText.js
@@ -1,13 +1,55 @@
 import React from "react";
 import { PortableText } from "@portabletext/react";
 import ParagraphText from "./ParagraphText";
-const myPortableTextComponent ={
-    block :{
-        normal:({children}) => <ParagraphText>
-            {children}
-        </ParagraphText>
-    }
-}
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import theme from "react-syntax-highlighter/dist/esm/styles/prism/darcula";
+import { Title } from "./Title";
+import sanityConfig from "../../../sanity-config,";
+import { getImage, getImageDimensions } from "@sanity/asset-utils";
+import customImage from "../../../../studio/schemas/objects/customImage";
+import { getSanityImageData } from "../../Utils/getSanityImageData";
+import { GatsbyImage } from "gatsby-plugin-image";
+const myPortableTextComponent = {
+  block: {
+    normal: ({ children }) => <ParagraphText>{children}</ParagraphText>,
+    h1: ({ children }) => <Title>{children}</Title>,
+  },
+  types: {
+    customCode: ({ value }) => (
+      <SyntaxHighlighter
+        style={theme}
+        className="bodyCode"
+        language={value.code.language}
+      >
+        {String(value.code.code).replace(/\n$/, "")}
+      </SyntaxHighlighter>
+    ),
+    customImage: ({ value }) => {
+      const imageData = getImage(value.asset, sanityConfig).asset;
+      const { width, height } = getImageDimensions(value);
+
+      const image = {
+        url: imageData.url,
+        height,
+        width,
+      };
+      const gatsbyImageData = getSanityImageData({
+        image,
+        layout: 'constrained',
+      });
+      return (
+        <GatsbyImage
+        image={gatsbyImageData}
+        alt={value.alt}
+        className="bodyImage"
+        >
+
+        </GatsbyImage>
+      )
+
+    },
+  },
+};
 function MyPortableText({ value }) {
   return <PortableText value={value} components={myPortableTextComponent} />;
 }
--- StarterFiles/web/src/templates/single-blog.js
+++ StarterFiles/web/src/templates/single-blog.js
@@ -1,8 +1,84 @@
 import React from "react";
+import MyPortableText from '../components/TypoGraphy/MyPortableText'
+import {BiCategory} from 'react-icons/bi'
+import ParagraphText from '../components/TypoGraphy/ParagraphText'
+import PageSpace from "../components/PageSpace";
+import {FiCalendar, FiUser} from 'react-icons/fi'
+import { format } from "date-fns";
+import { GatsbyImage } from "gatsby-plugin-image";
+import SEO from "../components/SEO";
+import { graphql } from "gatsby";
+import { Link } from "gatsby";
+import {Title} from '../components/TypoGraphy/Title'
+import { SingleBlogStyles } from "../styles/blog/SingleBlogStyles";
+export const postQuery = graphql`
+  query SingleBlogQuery($id: String!) {
+    sanityBlog(id: { eq: $id }) {
+      title
+      publishedAt
+      _rawBody
+      coverImage {
+        alt
+        asset {
+          gatsbyImageData
+        }
+      }
+      categories {
+        title
+        slug {
+          current
+        }
+      }
+      author {
+        name
+        slug {
+          current
+        }
+      }
+    }
+  }
+`;
+function singleBlog({ data }) {
+  const blog = data.sanityBlog;
+  return (
+    <SingleBlogStyles>
+      <SEO title={blog.title} />
+      <PageSpace top={80} bottom={100}>
+      <div className="container">
+        <div className="blog-header">
+          <GatsbyImage image={blog.coverImage.asset.gatsbyImageData} alt={blog.coverImage.alt} className="blog-cover-image" />
+          <Title className="title">{blog.title}</Title>
+          <ParagraphText className="publishedAt">
+          <FiCalendar/>
+          {format(new Date(blog.publishedAt), 'p, MMMM dd yyyy') }</ParagraphText>
+          <ParagraphText className="categoriesText">
+            <BiCategory/>
+            <span>
+              {blog.categories.map((item, index)=>(
+                <span key={item.slug.current}><Link to={`/categories/${item.slug.current}`}>
+                {item.title}
+                </Link>
+                {index < blog.categories.length - 1 ? ', ' : ''}
+                </span>
+              ))}
+            </span>
+          </ParagraphText>
+          <ParagraphText className="author">
+            <FiUser/>
+            <Link to={`/authors/${blog.author.slug.current}`}>
 
-function singleBlog(props) {
-    console.log(props);
-  return <div>single-blog</div>;
+              {blog.author.name}
+            </Link>
+          </ParagraphText>
+        </div>
+        <hr className="hr"/>
+        <div className="body">
+          <MyPortableText value={blog._rawBody}></MyPortableText>
+      </div>
+    </div>
+      </PageSpace>
+    </SingleBlogStyles>
+  );
 }
 
 export default singleBlog;
